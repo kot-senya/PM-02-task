68bed728e59c53e15824c98e47485e5f17670867
aush <kotkova_k@bk.ru>
2023-07-04 23:13:56 +0300

    --

--- Юнит тесты/UnitTest1.cs
+++ Юнит тесты/UnitTest1.cs
@@ -3,9 +3,24 @@ namespace Юнит_тесты
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Contains()
         {
+            //1-ое значение присутствует во 2 значении
+            Assert.Contains("", " ");
+        }
 
+        [Fact]
+        public void Equal()
+        {
+            //ожидаемое = результат
+            Assert.Equal(15, 7+8);
+        }
+
+        [Fact]
+        public void True()
+        {
+            //ожидаемое = результат
+            Assert.True(false);
         }
     }
 }
